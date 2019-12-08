7a1b76085d22c47dc2f7679648252a8bb12c420c
マッキヰ <macky1991@MBP.local>
2019-12-08 17:19:25 +0900

    create category pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,18 @@
 class HomeController < ApplicationController
+
+  def plamo
+  end
+
+  def toy
+  end
+
+  def car_model
+  end
+
+  def model_gun
+  end
+
+  def other
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
+
+  get "plamo" => "home#plamo"
+
+  get "toy" => "home#toy"
+
+  get "car_model" => "home#car_model"
+
+  get "model_gun" => "home#model_gun"
+
+  get "other" => "home#other"
+
 end
