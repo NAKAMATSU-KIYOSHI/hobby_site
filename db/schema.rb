c5950f9578e27d29bae2b1520eec63fc1b4ae003
マッキヰ <macky1991@MBP.local>
2019-12-15 03:05:27 +0900

    update login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_08_191612) do
+ActiveRecord::Schema.define(version: 2019_12_13_211331) do
 
   create_table "items", force: :cascade do |t|
     t.string "product_name"
@@ -43,8 +43,10 @@ ActiveRecord::Schema.define(version: 2019_12_08_191612) do
     t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
