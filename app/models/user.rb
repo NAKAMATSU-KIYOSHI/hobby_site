9dea9affa1f541376232ed01d8a97bb022923459
マッキヰ <macky1991@MBP.local>
2019-12-22 04:41:19 +0900

    put validation model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  validates :name,
+            presence: true,
+            uniqueness: { case_sensitive: false }
+
+  validates_format_of :name, with: /^[a-zA-Z0-9_¥.]*$/, multiline: true
+  validate :validate_name
+
   attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -8,7 +15,23 @@ class User < ApplicationRecord
     @login = login
   end
 
+def self.find_for_database_authentication(warden_conditions)
+  conditions = warden_conditions.dup
+  conditions[:email].downcase! if conditions[:email]
+  login = conditions.delete(:login)
+
+  where(conditions.to_hash).where(
+    ["lower(name) = :value OR lower(email) = :value",{ value: login:downcase }]
+    ).first
+end
+
   def login
     @login || self.name || self.email
   end
+
+  def validate_name
+    errors.add(:name, :invalid) if User.where(email: name).exists?
+  end
 end
+
+
