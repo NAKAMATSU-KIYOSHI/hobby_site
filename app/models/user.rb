b8767bc6e28f9f871345ad0785075daaf91d31a9
マッキヰ <macky1991@MBP.local>
2019-12-22 04:18:50 +0900

    edit model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.name || self.email
+  end
 end
