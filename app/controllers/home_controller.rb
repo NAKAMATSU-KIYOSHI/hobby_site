c7f3eb6b0779dae578c288dc0873e3eee8e8bc3c
マッキヰ <macky1991@MBP.local>
2019-12-24 02:35:03 +0900

    reset db and edit home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,18 +4,23 @@ class HomeController < ApplicationController
   end
 
   def plamo
+    @user = current_user
   end
 
   def toy
+    @user = current_user
   end
 
   def car_model
+    @user = current_user
   end
 
   def model_gun
+    @user = current_user
   end
 
   def other
+    @user = current_user
   end
 
 end
