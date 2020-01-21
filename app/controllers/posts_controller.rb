98b47bc5a1e4172bc8c927860b4eaa47d6c83564
マッキヰ <macky1991@MBP.local>
2020-01-21 17:39:47 +0900

    edit config and view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,10 @@ class PostsController < ApplicationController
 
   def show
      @post = Post.find_by(id:params[:id])
+      if @post.present?
+
+      else
+         puts "no post"
   end
 
   # GET /posts/new
---
+++ db/migrate/20200121082045_add_post_to_item_details.rb
@@ -0,0 +1,4 @@
+class AddPostToItemDetails < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
